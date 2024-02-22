3c466a934f1e12cefd778924d5c97ca3ea4e8f30
BeatrizMercado <bea.mercado.lop@gmail.com>
2024-02-22 16:32:33 +0100

    add contact form style

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,25 +1,26 @@
 import React from "react"
+import '../components/styles/Contact.css'
 
 const Contact = () => {
 
     return (
         <>
         <section>
-            <h1>Contacta con nosotros</h1>
             <form className="container-form">
-                <div>
+                <h1 className="form-title">Contacta con nosotros</h1>
+                
                 <label htmlFor="name">Nombre</label>
                 <input type="text" className="form-name"/>
-                </div>
-                <div>
+                
+                
                 <label htmlFor="email">Email</label>
                 <input type="email" className="form-email"/>
-                </div>
-                <div>
+                
+                
                 <label htmlFor="message">Mensaje</label>
-                <input type="mensaje" className="form-message"/>
-                </div>
-                <button type="submit">Enviar</button>
+                <input type="mensaje" rows='6' className="form-message"/>
+                
+                <button type="submit" className="send-button">Enviar</button>
             </form>
         </section>
         </>
