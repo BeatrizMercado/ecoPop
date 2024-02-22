a94e053185c093962b02f224001ec27f9df7cb47
BeatrizMercado <bea.mercado.lop@gmail.com>
2024-02-22 15:08:21 +0100

    add cart count to nav bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,6 @@
 import './styles/Nav.css';
 import logo from '../assets/logo.png';
+import cartIcon from '../assets/cartIcon.svg';
 
 const Nav = () => {
   return (
@@ -13,6 +14,11 @@ const Nav = () => {
             <a href=""><button>Galeria</button></a>
             <a href=""><button>Contacto</button></a>
             <a href=""><button>Sign Up</button></a>
+            <ul className='nav-cart'>
+              <li>
+                <img src={cartIcon} className='cart-img'/><span className='cart-count'>0</span>
+              </li>
+            </ul>
             {/* <a href=""><button>+ Subir Producto</button></a> */}
         </div>
     </nav>
